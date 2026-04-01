852b5f72fa651a5a1a8ee396e3077f75e807b859
Den-Zel1 <box1693@bk.ru>
2026-04-01 19:40:19 +0300

    Initial commit: root project structure

---
+++ my_fastapi_app/app/models.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from sqlalchemy import String, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+# Базовый класс для всех моделей
+class Base(DeclarativeBase):
+    pass
+
+
+class Organization(Base):
+    __tablename__ = "organizations"
+
+    # Mapped указывает тип для статического анализа (IDE перестанет подчеркивать)
+    # mapped_column задает настройки колонки в БД
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
+    tin: Mapped[str] = mapped_column(String(20), unique=True, index=True, nullable=False)
+    logo: Mapped[Optional[str]] = mapped_column(String(500))
+
+
