65203e4cd9cc09bd45951dbb789e54f224adde34
Den-Zel1 <box1693@bk.ru>
2026-04-02 21:13:50 +0300

    created connection to DB, added test POST and GET endpoints

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from fastapi import FastAPI
-from app.config import settings
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 
-app = FastAPI(title=settings.app_name)
+from app.database import SessionLocal, create_tables
+from app.models import Organization
 
-@app.get("/health")
-async def health_check():
-    return {"status": "ok", "app": settings.app_name}
+app = FastAPI()
 
-# Здесь в будущем будет: app.include_router(api_router)
+create_tables()
+
+
+# 1. Создаем функцию-зависимость (аналог контекстного менеджера)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db  # FastAPI передаст эту сессию в эндпоинт
+    finally:
+        db.close()  # Сессия закроется сама после завершения запроса
+
+
+# 2. Используем сессию в эндпоинте через Depends
+@app.get("/organizations/{org_id}")
+def read_organization(org_id: int, db: Session = Depends(get_db)):
+    # Работаем с базой через переменную db
+    org = db.query(Organization).filter(Organization.id == org_id).first()
+    if org is None:
+        raise HTTPException(status_code=404, detail="Organization not found")
+    return org
+
+
+@app.post("/organizations/")
+def create_organization(name: str, tin: str, db: Session = Depends(get_db)):
+    new_org = Organization(name=name, tin=tin)
+    db.add(new_org)
+    db.commit()
+    db.refresh(new_org)
+    return new_org
