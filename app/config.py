d5ff9103bee80262591dc04ff06dc5fc23503619
Den-Zel1 <box1693@bk.ru>
2026-04-02 10:09:13 +0300

    add correct docker compose file for PostgreSQL, Project structure normalized.

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     items_per_user: int = 50
 
     # Автоматически читает из .env
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="../.env")
 
 
 settings = Settings()
